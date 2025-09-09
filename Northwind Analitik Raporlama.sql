ff90a0cf1f4e48a39633f8d7a98c98c6e1ead4bd
Ceren Avcı <96480852+cernavcii@users.noreply.github.com>
2025-09-09 21:10:09 +0300

    Add files via upload

---
+++ Northwind Analitik Raporlama.sql
@@ -0,0 +1,53 @@
+﻿-- Northwind Analitik Raporlama
+
+-- 1.Yıllara Göre Satış Trendleri --
+
+SELECT YEAR(o.OrderDate) AS Yil,
+ROUND(SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)), 2) AS ToplamSatis
+FROM Orders o
+JOIN [Order Details] od ON o.OrderID = od.OrderID
+GROUP BY YEAR(O.OrderDate)
+ORDER BY Yil
+
+-- 2.Kategori Bazında Satış Analizi -- 
+
+SELECT c.CategoryName,
+ROUND(SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)), 2) AS ToplamSatis
+FROM [Order Details] od
+join Products p ON od.ProductID = p.ProductID
+join Categories c ON p.CategoryID =c.CategoryID
+group by c.CategoryName
+order by ToplamSatis desc
+
+
+-- 3.Bölgesel Satış Analizi -- 
+
+SELECT c.Country,
+ROUND(SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)), 2) AS ToplamSatis
+FROM Customers c 
+JOIN Orders o ON c.CustomerID =o.CustomerID
+JOIN [Order Details] od ON o.OrderID = od.OrderID
+GROUP BY c.Country
+ORDER BY ToplamSatis desc ;
+
+-- 4.Çalışan Performansı --
+
+SELECT e.FirstName +' '+ e.LastName AS Calisan,
+ROUND(SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)), 2) AS ToplamSatis,
+COUNT(DISTINCT o.OrderID) AS SiparisSayisi
+FROM Employees e
+JOIN Orders o ON e.EmployeeID = O.EmployeeID
+JOIN [Order Details] od ON o.OrderID = od.OrderID
+GROUP BY e.FirstName , e.LastName
+ORDER BY ToplamSatis desc ;
+
+-- 5.En Çok Sipariş Veren Müşteriler --
+
+SELECT c.CompanyName as Musteri,
+COUNT(o.OrderID) as SiparisSayisi,
+ROUND(SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)), 2) AS ToplamSatis
+FROM Customers c
+JOIN Orders o ON c.CustomerID = o.CustomerID
+JOIN [Order Details] od ON o.OrderID = od.OrderID
+GROUP BY c.CompanyName
+ORDER BY ToplamSatis desc ;
---
+++ Northwind Veri Keşfi (EDA).sql
@@ -0,0 +1,39 @@
+﻿
+-- Northwind Veri Keşfi (EDA)
+
+-- Kaç müşteri var?
+select count(*) from Customers  -- 91 adet müşteri bulunmakta.
+
+-- Kaç sipariş var ?
+select count(*) from Orders -- 830 adet sipraaiş bulunmakta.
+
+-- Orders tablosunda eksik müşteri ya da çalışan var mı?
+SELECT * 
+FROM Orders
+WHERE CustomerID IS NULL OR EmployeeID IS NULL -- Boş değer bulunmamaktadır.
+
+-- Order Details tablosunda eksik ürün var mı?
+SELECT * 
+FROM [Order Details]
+WHERE ProductID IS NULL  -- EKSİK DEĞER YOK.
+
+-- Temel İstatistikler
+
+-- Yıllara göre sipariş sayısı:
+
+SELECT YEAR(OrderDate) AS Yil ,COUNT(*) AS SiparisSayısı
+FROM Orders
+GROUP BY YEAR(OrderDate)
+ORDER BY Yil
+
+-- Kaç farklı ülkeye satış yapılmış?
+SELECT COUNT(DISTINCT Country) AS UlkeSayisi
+FROM Customers
+
+-- En çok satılan ürünler:
+SELECT TOP 10 p.ProductName , Sum(od.Quantity) as ToplamAdet
+FROM [Order Details] od
+join Products p on od.ProductID = p.ProductID
+GROUP BY p.ProductName
+ORDER BY ToplamAdet DESC
+
